db11979821de6c8d7373c96ecc7c9f188478a3b7
max4max <66414493+max4max@users.noreply.github.com>
2021-03-14 20:37:44 +0200

    feat: implement transform func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error()
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  function isControl(val) {
+    let controlArr = [
+      "--discard-next",
+      "--double-next",
+      "--double-prev",
+      "--discard-prev",
+    ]
+    if (controlArr.indexOf(val) >= 0) return true
+  }
+
+  return [...arr].reduce((resArr, item, index, sourceArr) => {
+    if (typeof item === "undefined") return resArr
+
+    if (
+      item === "--discard-next" &&
+      !isControl(sourceArr[index + 1]) &&
+      index + 2 <= sourceArr.length
+    ) {
+      sourceArr.splice(index + 1, 1)
+      return resArr
+    }
+
+    if (
+      item === "--double-next" &&
+      !isControl(sourceArr[index + 1]) &&
+      index + 2 <= sourceArr.length
+    ) {
+      resArr.push(sourceArr[index + 1])
+      return resArr
+    }
+
+    if (
+      item === "--double-prev" &&
+      !isControl(sourceArr[index - 1]) &&
+      index - 1 >= 0
+    ) {
+      resArr.push(sourceArr[index - 1])
+      return resArr
+    }
+
+    if (
+      item === "--discard-prev" &&
+      !isControl(sourceArr[index - 1]) &&
+      index - 1 >= 0
+    ) {
+      resArr.pop()
+      return resArr
+    }
+
+    if (!isControl(item)) resArr.push(item)
+    return resArr
+  }, [])
+}
