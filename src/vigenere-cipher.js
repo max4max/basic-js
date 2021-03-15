ed1ed2b896dcc6f1b0978d07485b64e555cded87
max4max <66414493+max4max@users.noreply.github.com>
2021-03-16 01:32:43 +0200

    implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,89 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  letterAray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+  square = (() => {
+    return this.letterAray.map((letter, i, array) => {
+      return array.slice(i).concat(array.slice(0, i))
+    })
+  })()
+
+  constructor(direct = true) {
+    this.direct = direct
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+    }
+
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+
+    let result = ""
+    let keyIndex = 0
+
+    if (message.length / key.length > 0)
+      key = key.repeat(Math.ceil(message.length / key.length))
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " " || !this.letterAray.includes(message[i])) {
+        result = result + message[i]
+        continue
+      }
+
+      result =
+        result +
+        this.square[this.letterAray.indexOf(message[i])][
+          this.letterAray.indexOf(key[keyIndex])
+        ]
+
+      keyIndex++
+    }
+
+    if (this.direct) {
+      return result
+    }
+
+    return result.split("").reverse().join("")
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+    }
+
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+
+    let result = ""
+    let keyIndex = 0
+
+    if (message.length / key.length > 0)
+      key = key.repeat(Math.ceil(message.length / key.length))
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " " || !this.letterAray.includes(message[i])) {
+        result = result + message[i]
+        continue
+      }
+
+      result =
+        result +
+        this.letterAray[
+          this.square[this.letterAray.indexOf(key[keyIndex])].indexOf(
+            message[i]
+          )
+        ]
+
+      keyIndex++
+    }
+
+    if (this.direct) {
+      return result
+    }
+
+    return result.split("").reverse().join("")
   }
 }
 
-module.exports = VigenereCipheringMachine;
+module.exports = VigenereCipheringMachine
