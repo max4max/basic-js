a720ef4955b07cc7da8d36621183f5f202fe20cf
max4max <66414493+max4max@users.noreply.github.com>
2021-03-15 02:46:32 +0200

    feat: implement repeater func

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let resultAdd = ""
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+  if (str === null) str = "null"
+  str = String(str)
+  if (options === undefined) return result
+  if (!("separator" in options)) options.separator = "+"
+
+  if ("addition" in options) {
+    if (options.addition === null) options.addition = "null"
+    options.addition = String(options.addition)
+    if (!("additionSeparator" in options)) options.additionSeparator = "|"
+
+    resultAdd = options.addition
+    if ("additionRepeatTimes" in options)
+      for (let i = 2; i <= options.additionRepeatTimes; i++) {
+        resultAdd = resultAdd + options.additionSeparator + options.addition
+      }
+  }
+
+  let result = str + resultAdd
+  console.log(result)
+  if ("repeatTimes" in options)
+    for (let i = 2; i <= options.repeatTimes; i++) {
+      result = result + options.separator + str + resultAdd
+    }
+
+  return result
+}
